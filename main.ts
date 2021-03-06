71df45c523cd3438577f04ad2ef3244d3ca44672
natumsol <natumsol@gmail.com>
2021-03-06 10:35:58 +0800

    feat: init

--- main.ts
+++ main.ts
@@ -1,112 +1,96 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Modal, Notice, Plugin, PluginSettingTab, Setting, MarkdownView } from 'obsidian';
+import { spacing } from 'pangu';
 
 interface MyPluginSettings {
-	mySetting: string;
+  autoSpacing: boolean;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+  autoSpacing: true,
+};
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  settings: MyPluginSettings;
 
-	async onload() {
-		console.log('loading plugin');
+  format(cm: CodeMirror.Editor): void {
+    const cursor = cm.getCursor();
+    cm.setValue(spacing(cm.getValue()));
+    cm.setCursor(cursor);
+  }
 
-		await this.loadSettings();
+  async onload() {
+    await this.loadSettings();
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+    this.addCommand({
+      id: 'pangu-format',
+      name: '为中英文字符间自动加入空格',
+      callback: () => {
+        const activeLeafView = this.app.workspace.activeLeaf.view;
+        if (activeLeafView instanceof MarkdownView) {
+          this.format(activeLeafView.sourceMode.cmEditor);
+        }
+      },
+    });
 
-		this.addStatusBarItem().setText('Status Bar Text');
+    this.registerCodeMirror(cm => {
+      this.settings.autoSpacing &&
+        cm.addKeyMap({
+          'Ctrl-S': this.format,
+          'Cmd-S': this.format,
+        });
+    });
 
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
+    this.addSettingTab(new SampleSettingTab(this.app, this));
+  }
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  onunload() {
+    console.log('unloading plugin');
+  }
 
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
 
 class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+  constructor(app: App) {
+    super(app);
+  }
 
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+  onOpen() {
+    let { contentEl } = this;
+    contentEl.setText('Woah!');
+  }
 
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
+  onClose() {
+    let { contentEl } = this;
+    contentEl.empty();
+  }
 }
 
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+  plugin: MyPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: MyPlugin) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		let {containerEl} = this;
+  display(): void {
+    let { containerEl } = this;
+    containerEl.empty();
 
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    new Setting(containerEl).setName('自动格式化').addToggle(toggle => {
+      toggle.setValue(this.plugin.settings.autoSpacing);
+      toggle.onChange(async value => {
+        this.plugin.settings.autoSpacing = value;
+        await this.plugin.saveSettings();
+        new Notice('按 CMD + R 或 F5 重新载入后生效。');
+      });
+    });
+  }
 }
